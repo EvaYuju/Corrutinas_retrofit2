f8e81857df5c1df2a76d15261d5347769b570f1f
EvaYuju <elopezmarquez@safareyes.es>
2023-02-26 16:25:36 +0100

    100% OK

--- app/src/main/java/com/ddi/corrutinas/MainActivity.kt
+++ app/src/main/java/com/ddi/corrutinas/MainActivity.kt
@@ -2,10 +2,12 @@ package com.ddi.corrutinas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ddi.corrutinas.databinding.ActivityMainBinding
+import com.google.android.material.internal.ViewUtils.hideKeyboard
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -72,9 +74,15 @@ para hacer la llamada del API.*/
                 } else {
                     showError()
                 }
+                // Llamar al método para ocultar el teclado
+                hideKeyboard()
             }//runOnUiThread
         }//Coroutine
     }//searchByName
+    private fun hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.viewRoot.windowToken, 0)
+    }
 
     private fun showError() {
         Toast.makeText(this, "Ha ocurrido un error", Toast.LENGTH_LONG).show()
